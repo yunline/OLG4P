b0657c23016e8e014642fd1f5d4c83fc3d696a45
yunline <yunline@foxmail.com>
2023-04-27 19:27:57 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as file:
 
 setup(
     name="OLG4P",
-    version="0.1",
+    version="0.2",
     url="https://github.com/yunline/OLG4P",
     description="Convert python script into python one-liner",
     long_description=long_description,
